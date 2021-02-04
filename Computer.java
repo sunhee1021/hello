ad4e278972f1f55e366a3a4b32c4fe12864a2493
Sunhee <sh089286@naver.com>
2021-02-04 18:47:29 +0900

    Computer.java

---
+++ Computer.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Computer {
+    
+    int price;
+    String bname;
+    int serialNumber;
+    String color;
+    
+    
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+
+}
